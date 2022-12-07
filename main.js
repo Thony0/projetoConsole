6bdcde2fbb7401171ecb5c9c5ec75f8a4fd2a01b
Thony0 <111467113+Thony0@users.noreply.github.com>
2022-12-07 11:31:08 -0300

    Add files via upload
    não

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("meu primeiro projeto no console");
+var PrimeiroNome="léu ";
+var UltimoNome="preguiça:D";
+var NomeCompleto=PrimeiroNome.concat(UltimoNome);
+console.log(NomeCompleto);
+
+var a=1;
+var b=2;
+var soma=a+b;
+console.log("soma: " +soma);
+var sub=a-b;
+console.log("subtração: " +sub);
+var mult=a*b
+console.log("multiplicação: " +mult);
+var divi=a/b
+console.log("divisão: " +divi);
